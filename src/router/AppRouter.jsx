10f6573c4a75def7e2a1afea2d202b8c8616c595
Jose Maria CLi <chema9703@gmail.com>
2023-06-10 03:10:57 -0600

    chore: Indent components

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,12 +5,11 @@ import { DevuConfPage, DevuHomePage } from "../pages"
 export const AppRouter = () => {
   return (
     <>
-        <Routes>
-            <Route path="home" element={<DevuHomePage></DevuHomePage>} ></Route>
-            <Route path="conf" element={<DevuConfPage></DevuConfPage>} ></Route>
-            <Route path="/*" element={<DevuConfPage/>}></Route>
-
-        </Routes>
+      <Routes>
+        <Route index element={<DevuHomePage />} />
+        <Route path="home" element={<DevuHomePage></DevuHomePage>} ></Route>
+        <Route path="conf" element={<DevuConfPage></DevuConfPage>} ></Route>
+      </Routes>
     </>
   )
 }
