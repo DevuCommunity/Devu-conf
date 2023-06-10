d1146213b4c73c389dc3393239a554adbc496383
Jose Maria CLi <chema9703@gmail.com>
2023-06-10 03:11:19 -0600

    chore: Prepare containers for the landing page

--- src/pages/DevuHomePage/DevuHomePage.jsx
+++ src/pages/DevuHomePage/DevuHomePage.jsx
@@ -1,6 +1,24 @@
+import { GradientText } from "../../cmponents/Shared/GradientText/GradientText.tsx";
 
 export const DevuHomePage = () => {
   return (
-    <div>DevuHomePage</div>
+    <div>
+      <GradientText type="title" gradientType="devu">
+        DEVU
+      </GradientText>
+      <div className="DevuPet"></div>
+      <div className="landingPage_description">
+        <div>
+          <span className="fredoka ">Devu</span> es la comunidad abierta donde  desarrolladores y
+          aficionados a la tecnología comparten su conocimiento y
+          brindan apoyo a quienes lo necesitan
+        </div>
+        <div>
+          <span>Únete en:</span>
+          <div>Facebook</div>
+          <div>Discord</div>
+        </div>
+      </div>
+    </div>
   )
 }
