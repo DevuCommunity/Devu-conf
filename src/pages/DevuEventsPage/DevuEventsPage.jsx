122e597efa4a6ff6b0270855b38516d74d8c2803
Jose Maria CLi <chema9703@gmail.com>
2023-06-17 00:09:36 -0600

    fix: Update prop name because it had a typo

--- src/pages/DevuEventsPage/DevuEventsPage.jsx
+++ src/pages/DevuEventsPage/DevuEventsPage.jsx
@@ -11,13 +11,13 @@ export const DevuEventsPage = () => {
         navigate("/conf");
     }
   return (
-    <motion.div className="events-container" 
+    <motion.div className="events-container"
     initial={{ opacity: 0}}
-    animate={{ opacity: 1 }}    
+    animate={{ opacity: 1 }}
 
     >
         <DescriptionEventCard imgUrl={"/assets/networking.png"} title={"Devu.conf"} description={'Es un evento de la comunidad y para la comunidad, que reúne a los mejores developers de la región de Orizaba Veracruz para compartir su conocimiento mediante charlas y talleres. con el objetivo de motivar y ayudar las nuevas generaciones de developers.'}>
-        <Button boderColor="purple" handleClick={handleClickDesc}>
+        <Button borderColor="purple" handleClick={handleClickDesc}>
                 Proximo Devu.conf
               </Button>
         </DescriptionEventCard>
