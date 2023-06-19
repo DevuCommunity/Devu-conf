2427e18175860efa74b817bf8874ebadc6eb2544
Jose Maria CLi <chema9703@gmail.com>
2023-06-18 18:51:42 -0600

    fix: center the events page components after the layout updates

--- src/pages/DevuEventsPage/DevuEventsPage.jsx
+++ src/pages/DevuEventsPage/DevuEventsPage.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import { DescriptionEventCard } from "../../cmponents/Event/DescriptionEventCard/DescriptionEventCard";
 import { Button } from "../../cmponents/Shared";
-
+import "./DevuEventsPage.css";
 
 export const DevuEventsPage = () => {
     const navigate = useNavigate();
@@ -11,16 +11,18 @@ export const DevuEventsPage = () => {
         navigate("/conf");
     }
   return (
-    <motion.div className="events-container"
-    initial={{ opacity: 0}}
-    animate={{ opacity: 1 }}
-
+    <motion.div
+      id="EventsPage__container"
+      initial={{ opacity: 0}}
+      animate={{ opacity: 1 }}
     >
+      <div id="EventsPage__group-wrapper">
         <DescriptionEventCard imgUrl={"/assets/networking.png"} title={"Devu.conf"} description={'Es un evento de la comunidad y para la comunidad, que reúne a los mejores developers de la región de Orizaba Veracruz para compartir su conocimiento mediante charlas y talleres. con el objetivo de motivar y ayudar las nuevas generaciones de developers.'}>
         <Button borderColor="purple" handleClick={handleClickDesc}>
                 Proximo Devu.conf
               </Button>
         </DescriptionEventCard>
+      </div>
     </motion.div>
   )
 }
