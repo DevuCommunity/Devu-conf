11cf0025b285515ddc9fa71e16ebb18e4437382b
odon <odon.lozada@axity.com>
2023-06-18 20:52:21 -0600

    chenge font size to scale of 8px

--- src/pages/DevuEventsPage/DevuEventsPage.jsx
+++ src/pages/DevuEventsPage/DevuEventsPage.jsx
@@ -1,28 +1,31 @@
 import { motion } from "framer-motion";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { DescriptionEventCard } from "../../cmponents/Event/DescriptionEventCard/DescriptionEventCard";
 import { Button } from "../../cmponents/Shared";
 import "./DevuEventsPage.css";
 
 export const DevuEventsPage = () => {
-    const navigate = useNavigate();
-
-    const handleClickDesc = () => {
-        navigate("/conf");
-    }
   return (
     <motion.div
       id="EventsPage__container"
-      initial={{ opacity: 0}}
+      initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
     >
       <div id="EventsPage__group-wrapper">
-        <DescriptionEventCard imgUrl={"/assets/networking.png"} title={"Devu.conf"} description={'Es un evento de la comunidad y para la comunidad, que reúne a los mejores developers de la región de Orizaba Veracruz para compartir su conocimiento mediante charlas y talleres. con el objetivo de motivar y ayudar las nuevas generaciones de developers.'}>
-        <Button borderColor="purple" handleClick={handleClickDesc}>
-                Proximo Devu.conf
-              </Button>
+        <DescriptionEventCard
+          imgUrl={"/assets/networking.png"}
+          title={"Devu.conf"}
+          description={
+            "Es un evento de la comunidad y para la comunidad, que reúne a los mejores developers de la región de Orizaba Veracruz para compartir su conocimiento mediante charlas y talleres. con el objetivo de motivar y ayudar las nuevas generaciones de developers."
+          }
+        >
+          <Link to="/conf">
+            <Button variant="bordered" borderColor="purple">
+              Proximo Devu.conf
+            </Button>
+          </Link>
         </DescriptionEventCard>
       </div>
     </motion.div>
-  )
-}
+  );
+};
